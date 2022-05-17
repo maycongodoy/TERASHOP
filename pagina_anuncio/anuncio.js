d2544ce4c75a3cf6b481c0092ddec38bf058e807
Maycon Godoy <mykooon@hotmail.com>
2022-05-16 22:03:33 -0300

    Limpei a função

--- pagina_anuncio/anuncio.js
+++ pagina_anuncio/anuncio.js
@@ -1,15 +1,8 @@
-/* document.getElementById('image1').addEventListener('click', trocarFoto)
-let imageTroca = document.getElementById('fotoPrincipal')
 
-function trocarFoto (imageTroca) {
-    this.style.background-imageurl(imagens/banner2.png);
-}
-
- */
 let imagemPrincipal = document.getElementById('meRemova')
 console.log(imagemPrincipal)
 
-let imagemInicial = 1;
+
 
 function trocar (numeroImagem) {
     console.log(numeroImagem)
