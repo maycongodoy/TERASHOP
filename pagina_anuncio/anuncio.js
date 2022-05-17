c970277fc18ed0920e87032efee1ac1a79232f2b
Maycon Godoy <mykooon@hotmail.com>
2022-05-16 21:47:21 -0300

    Alterei a função

--- pagina_anuncio/anuncio.js
+++ pagina_anuncio/anuncio.js
@@ -11,20 +11,8 @@ console.log(imagemPrincipal)
 
 let imagemInicial = 1;
 
-function trocarImagem (numeroImagem) {
-    if(numeroImagem === undefined) {
-        if(imagemInicial === 1) {
-            imagemInicial = 2;
-            imagemPrincipal.src ='imagens/banner2.png'
-        }else if (imagemInicial === 2) {
-            imagemInicial = 3;
-            imagemPrincipal.src ='imagens/D12-2761-002_zoom3.png'
-        } else if (imagemInicial === 3) {
-            imagemInicial = 1;
-            imagemPrincipal.src ='imagens/banner.jpg'
-        }else {
-            imagemPrincipal.src = `imagens/${numeroImagem}.png`
-        }
-    }
+function trocar (numeroImagem) {
+    console.log(numeroImagem)
+        imagemPrincipal.src = numeroImagem
+       
 }
-s
